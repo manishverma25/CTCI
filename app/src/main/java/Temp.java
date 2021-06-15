8393d4785fe5a1929376424e572d4fb2d4ac11c1
Manish Verma <a_manish.verma@airtelworld.com>
2021-06-15 12:20:09 +0530

    Initial commit

---
+++ app/src/main/java/Temp.java
@@ -0,0 +1,9 @@
+public class Temp {
+
+
+
+    public static void main(String args[]) {
+        int n = 2;
+        System.out.println(" n = " + n + " ,nos of total non sonsective 1 are" );
+    }
+}
